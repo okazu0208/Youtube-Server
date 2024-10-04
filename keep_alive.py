685c85b18bcd17ee768b5f56d022b03d3c9168a2
okazu <kazuya_y_y_y@outlook.jp>
2024-10-04 16:10:51 +0900

    and add

--- keep_alive.py
+++ keep_alive.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from threading import Thread
+from pytube import YouTube
+from io import BytesIO
+import base64
 
 app = Flask('')
 
 @app.route('/')
 def home():
+    yt = YouTube('https://www.youtube.com/watch?v=aXE4aMjbfww')
+    stream = yt.streams.first()
+    buffer=BytesIO()
+    stream.stream_to_buffer(buffer)
+    buffer.seek(0)
+    base64.b64encode(buffer.read()).decode()
     return "I'm alive"
 
 def run():
